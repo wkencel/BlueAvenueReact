99ade11c5d1835155cdc1c5202b169d924acd226
Will Kencel <wkencel1@gmail.com>
2021-07-09 20:05:20 -0700

    linting changes

--- src/components/TimeInput.js
+++ src/components/TimeInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
 // const useStyles = makeStyles((theme) => ({
