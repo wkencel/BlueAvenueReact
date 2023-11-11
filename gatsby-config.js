e491da04170c547f2de3ae5f48d0b9cd35b1b4b0
Will Kencel <wkencel1@gmail.com>
2023-11-11 14:34:12 -0800

    updated gatsby-config
    added proper thumbnail image for site and updated site Metadata info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Blue Avenue Groove Wedding band site',
+    author: 'Will Kencel',
+    description: 'NY wedding band - serving NYC, hudson valley with amazing wedding entertainment',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    "gatsby-plugin-webpack-bundle-analyser-v2",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -15,7 +16,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/building-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
