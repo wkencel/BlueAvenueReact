54f2584e21759b08bd38151270c06a267db98dfd
Will Kencel <wkencel1@gmail.com>
2023-11-11 21:51:11 -0800

    fixed styling
    video is now the proper height when playing

--- src/components/YoutubeLiteComponent.js
+++ src/components/YoutubeLiteComponent.js
@@ -25,7 +25,7 @@ export default function LightYouTubeEmbed({ id }) {
             {videoPlayed && !stopPlay ? (
                 <iframe
                     width="100%"
-                    height="100%"
+                    height="300px"
                     src={videoSrc}
                     title="Video"
                     allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
