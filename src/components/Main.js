5394f00fcda4c7dda9874e3630ce3c051bcf7795
Will Kencel <wkencel1@gmail.com>
2023-11-11 21:44:55 -0800

    refactored Main component
    changed from object to functional component to utilize hooks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,96 +1,97 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useContext } from 'react'
 import ContactForm from './Contact.js'
 import WhoWeAre from './WhoWeAre.js'
 import Media from './Media.js'
 import EventServices from './EventServices.js'
 import Reviews from './Reviews.js'
+import { globalContext } from "../context/context"
 
-class Main extends React.Component {
-  render() {
-    let close = (
-      <div
-        className="close"
-        onClick={() => {
-          this.props.onCloseArticle()
-        }}
-      ></div>
-    )
+function Main({ article, articleTimeout, onCloseArticle, timeout, setWrapperRef }) {
 
-    return (
-      <div
-        ref={this.props.setWrapperRef}
-        id="main"
-        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
-      >
-        <article
+  const { setStopPlay, stopPlay } = useContext(globalContext);
+  let close = (
+    <div
+      className="close"
+      onClick={() => {
+        setStopPlay(true)
+        onCloseArticle();
+      }}
+    ></div>
+  );
+
+  return (
+    <div
+      ref={setWrapperRef}
+      id="main"
+      style={timeout ? { display: 'flex' } : { display: 'none' }}
+    >
+      <article
         id="event-services"
-        className={`${this.props.article === 'event-services' ? 'active' : ''} ${
-          this.props.articleTimeout ? 'timeout' : ''
+        className={`${article === 'event-services' ? 'active' : ''} ${
+          articleTimeout ? 'timeout' : ''
         }`}
         style={{ display: 'none' }}
-        >
-          <EventServices />
-          {close}
-        </article>
-      
-        <article
-        id="media"
-        className={`${this.props.article === 'media' ? 'active' : ''} ${
-          this.props.articleTimeout ? 'timeout' : ''
-        }`}
-        style={{ display: 'none' }}
-        >
-          <Media />
-          {close}
-        </article>
-      
-        <article
-        id="about"
-        className={`${this.props.article === 'about' ? 'active' : ''} ${
-          this.props.articleTimeout ? 'timeout' : ''
-        }`}
-        style={{ display: 'none' }}
-        >
-        <WhoWeAre />
-          {close}
-        </article>
-      
-        <article
-          id="contact"
-          className={`${this.props.article === 'contact' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
+      >
+        <EventServices />
+        {close}
+      </article>
+    
+      <article
+          id="media"
+          className={`${article === 'media' ? 'active' : ''} ${
+            articleTimeout ? 'timeout' : ''
           }`}
           style={{ display: 'none' }}
-          >
-          <h2 className="major">Contact</h2>
-          <ContactForm />
-          {close}
-        </article>
-
-        <article
-        id="contact"
-        className={`${this.props.article === 'reviews' ? 'active' : ''} ${
-          this.props.articleTimeout ? 'timeout' : ''
+      >
+        <Media />
+        {close}
+      </article>
+    
+      <article
+        id="about"
+        className={`${article === 'about' ? 'active' : ''} ${
+          articleTimeout ? 'timeout' : ''
         }`}
         style={{ display: 'none' }}
-        >
+      >
+        <WhoWeAre />
+        {close}
+      </article>
+    
+      <article
+        id="contact"
+        className={`${article === 'contact' ? 'active' : ''} ${
+          articleTimeout ? 'timeout' : ''
+        }`}
+        style={{ display: 'none' }}
+      >
+        <h2 className="major">Contact</h2>
+        <ContactForm />
+        {close}
+      </article>
+
+      <article
+        id="contact"
+        className={`${article === 'reviews' ? 'active' : ''} ${
+          articleTimeout ? 'timeout' : ''
+        }`}
+        style={{ display: 'none' }}
+      >
         <h2 className="major">Reviews</h2>
         <Reviews />
         {close}
       </article>
-      </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 Main.propTypes = {
-  route: PropTypes.object,
   article: PropTypes.string,
   articleTimeout: PropTypes.bool,
   onCloseArticle: PropTypes.func,
   timeout: PropTypes.bool,
   setWrapperRef: PropTypes.func.isRequired,
-}
+};
 
-export default Main
+export default Main;
