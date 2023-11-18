71cfcf634bbf4a461141ca53c65f30ecc48be8de
Will Kencel <wkencel1@gmail.com>
2023-11-18 13:42:43 -0800

    dependency changes
    added and removed dependency packages for performance, removed unneccessary dependencies. modified gatsby-config and gatsby-node files for performance

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,20 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    "gatsby-plugin-webpack-bundle-analyser-v2",
+    // {
+    //   resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
+    //   options: {
+    //     analyzerMode: 'server',
+    //     analyzerPort: '8888',
+    //   },
+    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/`,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -16,7 +29,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/building-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/building-icon.webp', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
