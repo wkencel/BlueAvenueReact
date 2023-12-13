756ce8c076e013b3aa73dacdcfd1610a44c28865
Will Kencel <wkencel1@gmail.com>
2023-12-13 09:53:07 -0500

    updated bk nyc page links

--- src/pages/brooklyn-wedding-bands.js
+++ src/pages/brooklyn-wedding-bands.js
@@ -3,53 +3,119 @@ import Layout from '../components/layout'; // Assuming you have a Layout compone
 
 const BrooklynWeddings = () => {
   return (
-    <Layout>
-      <h1>Blue Avenue Music: Unforgettable Live Wedding Bands in Brooklyn</h1>
-      <p>Welcome to the home of Brooklyn's finest wedding bands, where we bring a blend of authenticity, energy, and musical versatility to your special day in one of New York's most vibrant boroughs.
-
-
-About Our Brooklyn Wedding Bands (h2)
-
-Our Essence: As the leading providers of wedding bands in Brooklyn, we embody the unique spirit and diversity of Brooklyn in every performance. Our bands are a mosaic of the borough's rich cultural tapestry, offering an unparalleled musical experience.
-
-Our Mission: Our mission is to elevate your wedding day with top-tier music, transforming it into an unforgettable celebration that resonates with the soul of Brooklyn.
-
-
-Our Services 
-
-Dynamic Band Sizes: From intimate quartets to grand 12-piece ensembles, our wedding bands in Brooklyn cater to every scale and style. We ensure the highest quality music, tailored to fit the ambiance of your celebration.
-
-Custom Music Playlists: Reflecting Brooklyn's eclectic vibe, our bands specialize in a broad range of genres. We work with you to create a playlist that not only entertains but also tells your unique love story.
-
-State-of-the-Art Audio: Quality sound is paramount. Our bands use professional audio setups, managed by experienced AV engineers, to deliver crystal-clear sound that elevates every performance.
-
-Engaging MC Services: Our bands do more than just play music; they enhance the overall experience by proficiently handling MC duties, including announcing key moments like dances and cake cutting.
-
-Diverse Music Genres: Specializing in genres like Jazz, Funk, Soul, Pop, R&B, and Rock, our bands capture the diverse musical landscape of Brooklyn, ensuring a vibrant and memorable experience for you and your guests.
-
-
-Why Choose Our Brooklyn Wedding Bands
-
-Local Brooklyn Flavor: Our bands are not just in Brooklyn; they are of Brooklyn. Their performances capture the unique energy and soul of the borough, making them a top choice for wedding bands in Brooklyn.
-
-Tailored to Your Style: Understanding the importance of personalization, we tailor every aspect of our performance to align with your vision and wedding theme.
-
-Booking Process
-Easy and Personal Consultation: Your journey with us starts with a simple conversation. We listen, we understand, and we craft a musical experience that mirrors your personal taste and wedding theme.
-
-Customizable Packages: Our packages are designed to cater to your specific needs, whether you're looking for a band for just the reception or a full-day music experience.
-
-FAQ
-We can add FAQ (please let me know)
-
-Contact Information
-Book Your Brooklyn Wedding Band Today: Ready to experience the best of Brooklyn's wedding music? Reach out to us via our contact form, email, or phone. Let's make your wedding day as unique and vibrant as Brooklyn itself!
-
-
-Call to Action
-Book Your Date Now: Don’t miss the chance to have Blue Avenue Music be a part of your special day. Contact us today to secure your date and experience the best of live wedding music in Brooklyn.
-</p>
-    </Layout>
+    <>
+      <Layout>
+        <h1>Blue Avenue Music</h1>
+        <div style={{ "margin-left": "2%" }}>
+          <h2>The Most Unforgettable Live Wedding Band in Brooklyn</h2>
+          <p>
+            Welcome to the home of Brooklyn's finest wedding band, where we
+            bring a blend of authenticity, energy, and musical versatility to
+            your special day in one of New York's most vibrant boroughs.
+          </p>
+        </div>
+        <br />
+        <h2>About Your Brooklyn Wedding Band: Blue Avenue Groove</h2>
+        <div style={{ "margin-left": "2%" }}>
+          <h3>Our Essence: </h3>
+          <p>
+            As the leading wedding band in Brooklyn, we embody the unique spirit
+            and diversity of Brooklyn in every performance. Our bands are a
+            mosaic of the borough's rich cultural tapestry, offering an
+            unparalleled musical experience.
+          </p>
+          <h3>Our Mission: </h3>
+          <p>
+            Our mission is to elevate your wedding day with top-tier music,
+            transforming it into an unforgettable celebration that resonates
+            with the soul of Brooklyn.
+          </p>
+        </div>
+        <br />
+        <h2>Our Services</h2>
+        <div style={{ "margin-left": "2%" }}>
+          <h3> Dynamic Band Sizes: </h3>
+          <p>
+            From intimate quartets to grand 12-piece ensembles, our wedding
+            bands in Brooklyn cater to every scale and style. We ensure the
+            highest quality music, tailored to fit the ambiance of your
+            celebration.
+          </p>
+          <h3>Custom Music Playlists:</h3>
+          <p>
+            Reflecting Brooklyn's eclectic vibe, our bands specialize in a broad
+            range of genres. We work with you to create a playlist that not only
+            entertains but also tells your unique love story.
+          </p>
+          <h3>State-of-the-Art Audio:</h3>
+          <p>
+            Quality sound is paramount. Our band uses professional audio setups,
+            managed by experienced AV engineers, to deliver crystal-clear sound
+            that elevates every performance.
+          </p>
+          <h3>Engaging MC Services:</h3>
+          <p>
+            Our band does more than just play music; we enhance the overall
+            experience by proficiently handling MC duties, including announcing
+            key moments like dances and cake cutting.
+          </p>
+          <h3>Diverse Music Genres:</h3>
+          <p>
+            Specializing in genres like Jazz, Funk, Soul, Pop, R&B, and Rock,
+            Blue Avenue Groove captures the diverse musical landscape of
+            Brooklyn, ensuring a vibrant and memorable experience for you and
+            your guests.
+          </p>
+        </div>
+        <br />
+        <h2>Why Choose Blue AVenue Groove for your Brooklyn Wedding Band</h2>
+        <div style={{ "margin-left": "2%" }}>
+          <h3>Local Brooklyn Flavor:</h3>
+          <p>
+            Our band is not just in Brooklyn; we are of Brooklyn. Our
+            performances capture the unique energy and soul of the borough,
+            making us a top choice for wedding bands in Brooklyn.
+          </p>
+          <h3>Tailored to Your Style:</h3>
+          <p>
+            Understanding the importance of personalization, we tailor every
+            aspect of our performance to align with your vision and wedding
+            theme.
+          </p>
+        </div>
+        <br />
+        <h2>Booking Process</h2>
+        <div style={{ "margin-left": "2%" }}>
+          <h3>Consultation: </h3>
+          <p>
+            Your journey with us starts with a simple conversation. We listen,
+            we understand, and we craft a musical experience that mirrors your
+            personal taste and wedding theme.
+          </p>
+          <h3>Customizable Packages:</h3>
+          <p>
+            Our packages are designed to cater to your specific needs, whether
+            you're looking for a band for just the reception or a full-day music
+            experience.
+          </p>
+        </div>
+        <br />
+        <h2>Book Your Brooklyn Wedding Band Today:</h2>
+        <p>
+          Ready to experience the best of Brooklyn? Reach out to us via our
+          contact form, email, or phone. Let's make your wedding day as unique
+          and vibrant as Brooklyn itself! Don’t miss the chance to have Blue
+          Avenue Groove be a part of your special day. Contact us today to
+          secure your date and experience the best of live wedding music in
+          Brooklyn.
+        </p>
+        <h3>Send us a message to Book</h3>
+        <ContactForm />
+        <Link to="/" style={{ fontSize: "24px" }}>
+          Home
+        </Link>
+      </Layout>
+    </>
   );
 };
 
--- src/pages/wedding-bands-nyc.js
+++ src/pages/wedding-bands-nyc.js
@@ -3,54 +3,142 @@ import Layout from '../components/layout'; // Assuming you have a Layout compone
 
 const NYCWeddings = () => {
   return (
-    <Layout>
-      <h1>Blue Avenue Music: Unforgettable Live Wedding Bands in NYC</h1>
-      <p>
-
-Welcome to Blue Ave Music, where the essence of live music meets the magic of matrimony in the heart of New York City. Our bands are not just performers; they are storytellers dedicated to turning your wedding day into an unforgettable celebration.
-
-About Our NYC Wedding Bands (h2)
-
-Our Philosophy: In the bustling city of New York, Blue Avenue Music stands out with a belief that music is a unifying force. Our team, hailing from diverse backgrounds, forms a unique family of musicians. Together, we're changing the narrative of wedding entertainment in NYC.
-
-Our Vision: Beyond traditional tunes, we're crafting a new kind of wedding band experience. It's about music that touches the soul and brings people together, transforming your special day into an extraordinary lifetime memory.
-
-Our Services 
-
-Customizable Band Options: At Blue Avenue Music, we offer a dynamic range of band sizes to perfectly fit your wedding's atmosphere. Our offerings start with a 6-piece band and can expand up to a 12-piece ensemble, ensuring we deliver four hours of the highest quality music in NYC. The base band includes two vocalists, guitar, keys, bass, and drums, providing a rich, full sound that sets the tone for your special day.
-
-Additional Instrument Options: For a more lavish musical experience, we offer the option to include additional instruments such as tenor sax, trumpet, alto sax, and trombone. These additions not only enhance the richness of the sound but also add a layer of sophistication to your wedding entertainment.
-
-Professional Audio Setup: Every performance by Blue Avenue Music is supported by a professional audio setup, complete with an experienced AV Engineer. This ensures that the sound quality is impeccable, allowing every note to be heard crisply and clearly, enhancing the overall experience.
-
-Personalized Song Requests: We understand the importance of personal touches at your wedding. Blue Avenue Music is pleased to offer the performance of three song requests, which often include the first dance and parents' dances. These special songs are more than just music; they're the soundtrack to your most cherished moments.
-
-Expert MC Services: Beyond music, our band also provides MC services to ensure your reception runs smoothly. We'll handle the announcements for dances, cake cutting, and other key moments, keeping your guests informed and engaged throughout the celebration.
-
-Specialized Music Genres: Our repertoire is diverse and rich, specializing in genres like Funk, Soul, Pop/Top 40, R&B, and Rock. This variety ensures that we cater to a wide range of musical tastes, making your wedding celebration not just an event, but a memorable experience for everyone involved.
-
-
-Why Choose our NYC Wedding band? (h2)
-
-Experience in NYC Weddings: Navigating the vibrant and diverse wedding scene of NYC is our forte. Our bands are seasoned in performing at various venues, understanding the pulse of the city like no other.
-
-Professional and Passionate Musicians: We are more than just musicians; we are artists who pour our heart and soul into every performance, ensuring your wedding is filled with moments of musical bliss.
-
-Booking Process
-Easy and Personal Consultation: Your journey with us starts with a simple conversation. We listen, we understand, and we craft a musical experience that mirrors your personal taste and wedding theme.
-
-Customizable Packages: Our packages are designed to cater to your specific needs, whether you're looking for a band for just the reception or a full-day music experience.
-
-FAQ
-We can add FAQ (please let me know)
-
-Contact Information
-Get in Touch: Ready to make your wedding a day to remember with the best live wedding band in NYC? Reach out to us via our contact form, email, or phone. Follow our social media pages for more insights into our performances and updates.
-
-Call to Action
-Book Your Date Now: Don’t miss the chance to have Blue Avenue Music be a part of your special day. Contact us today to secure your date and experience the best of live wedding music in NYC.
-</p>
-    </Layout>
+    <>
+      <Layout>
+        <h1>Blue Avenue Groove Music</h1>
+        <div style={{ "margin-left": "2%" }}>
+          <h2>The Most Unforgettable Live Wedding Band in NYC</h2>
+          <p>
+            Welcome to Blue Avenue Groove, where the essence of live music meets
+            the magic of matrimony in the heart of New York City. Our musicians
+            are not just performers; they are storytellers dedicated to turning
+            your wedding day into an unforgettable celebration.
+          </p>
+        </div>
+        <br />
+        <h2>About Blue Avenue Groove, your NYC Wedding Band</h2>
+        <div style={{ "margin-left": "2%" }}>
+          <h3>Our Philosophy:</h3>
+          <p>
+            In the bustling city of New York, Blue Avenue Groove stands out with
+            a belief that music is a unifying force. Our team, hailing from
+            diverse backgrounds, forms a unique family of musicians. Together,
+            we're changing the narrative of wedding entertainment in NYC.
+          </p>
+          <h3>Our Vision:</h3>
+          <p>
+            Beyond traditional tunes, we're crafting a new kind of wedding band
+            experience. It's about music that touches the soul and brings people
+            together, transforming your special day into an extraordinary
+            lifetime memory.
+          </p>
+        </div>
+        <br />
+        <h2>Our Services</h2>
+        <div style={{ "margin-left": "2%" }}>
+          <h3>Customizable Band Options: </h3>
+          <p>
+            At Blue Avenue Groove, we offer a dynamic range of band sizes to
+            perfectly fit your wedding's atmosphere. Our offerings start with a
+            6-piece band and can expand up to a 12-piece ensemble, ensuring we
+            deliver four hours of the highest quality music in NYC. The base
+            band includes two vocalists, guitar, keys, bass, and drums,
+            providing a rich, full sound that sets the tone for your special
+            day.
+          </p>
+          <h3>Additional Instrument Options:</h3>
+          <p>
+            For a more lavish musical experience, we offer the option to include
+            additional instruments such as another vocalist, tenor sax, trumpet,
+            alto sax, and trombone. These additions not only enhance the
+            richness of the sound but also add another layer of sophistication
+            to your wedding entertainment.
+          </p>
+          <h3>Professional Audio Setup: </h3>
+          <p>
+            Every performance by Blue Avenue Groove is supported by a
+            professional audio setup, complete with an experienced AV Engineer.
+            This ensures that the sound quality is impeccable, allowing every
+            note to be heard crisply and clearly, enhancing the overall
+            experience.
+          </p>
+          <h3>Personalized Song Requests: </h3>
+          <p>
+            We understand the importance of personal touches at your wedding.
+            Blue Avenue Groove is pleased to offer the performance of three song
+            requests, which often include the first dance and parents' dances.
+            These special songs are more than just music; they're the soundtrack
+            to your most cherished moments.
+          </p>
+          <h3>Expert MC Services: </h3>
+          <p>
+            Beyond music, our band also provides MC services to ensure your
+            reception runs smoothly. We'll handle the announcements for dances,
+            cake cutting, and other key moments, keeping your guests informed
+            and engaged throughout the celebration.
+          </p>
+          <h3>Specialized Music Genres: </h3>
+          <p>
+            Our repertoire is diverse and rich, specializing in genres like
+            Funk, Soul, Pop/Top 40, R&B, and Rock. This variety ensures that we
+            cater to a wide range of musical tastes, making your wedding
+            celebration not just an event, but a memorable experience for
+            everyone involved.
+          </p>
+        </div>
+        <br />
+        <h2>Why Choose Blue Avenue Groove for your NYC Wedding band?</h2>
+        <div style={{ "margin-left": "2%" }}>
+          <h3>Experience in NYC Weddings:</h3>
+          <p>
+            Navigating the vibrant and diverse wedding scene of NYC is our
+            forte. Our bands are seasoned in performing at various venues,
+            understanding the pulse of the city like no other.
+          </p>
+          <h3>Professional and Passionate Musicians: </h3>
+          <p>
+            We are more than just musicians; we are artists who pour our heart
+            and soul into every performance, ensuring your wedding is filled
+            with moments of musical bliss.
+          </p>
+        </div>
+        <br />
+        <h2>Booking Process</h2>
+        <div style={{ "margin-left": "2%" }}>
+          <h3>Consultation: </h3>
+          <p>
+            Your journey with us starts with a simple conversation. We listen,
+            we understand, and we craft a musical experience that mirrors your
+            personal taste and wedding theme.
+          </p>
+          <h3>Customizable Packages:</h3>
+          <p>
+            Our packages are designed to cater to your specific needs, whether
+            you're looking for a band for just the reception or a full-day music
+            experience.
+          </p>
+          <h3>Get in Touch: </h3>
+          <p>
+            Ready to make your wedding a day to remember with the best live
+            wedding band in NYC? Reach out to us via our contact form, email, or
+            phone. Follow our social media pages for more insights into our
+            performances and updates.
+          </p>
+        </div>
+        <br />
+        <h2>Book Your Date Now:</h2>
+        <p>
+          Don’t miss the chance to have Blue Avenue Groove be a part of your
+          special day. Contact us today to secure your date and experience the
+          best of live wedding music in NYC.
+        </p>
+        <h3>Send us a message to Book</h3>
+        <ContactForm />
+        <Link to="/" style={{ fontSize: "24px" }}>
+          Home
+        </Link>
+      </Layout>
+    </>
   );
 };
 
