a6968e0aa920c3fd605e724dd8438b9267f7cbf0
Will Kencel <wkencel1@gmail.com>
2024-02-10 18:04:35 -0800

    resolved warnings on youtube lite component

--- src/components/YoutubeLiteComponent.js
+++ src/components/YoutubeLiteComponent.js
@@ -1,42 +1,80 @@
-import React, { useState, useContext } from 'react';
-import { globalContext } from '../context/context';
+import React, { useState, useContext, useEffect } from "react";
+import { globalContext } from "../context/context";
 
 export default function LightYouTubeEmbed({ id }) {
-    const [videoPlayed, setVideoPlayed] = useState(false);
+  const [videoPlayed, setVideoPlayed] = useState(false);
+  const videoSrc = `https://www.youtube.com/embed/${id}?autoplay=1`;
+  const { stopPlay, setStopPlay } = useContext(globalContext);
 
-    const videoSrc = `https://www.youtube.com/embed/${id}?autoplay=1`;
+  // Add effect to set setStopPlay when the component is mounted
+  useEffect(() => {
+    setStopPlay(false);
+  }, [setStopPlay]);
 
-    const { stopPlay, setStopPlay } = useContext(globalContext);
-    setStopPlay(false)
+  const handleVideoTrigger = (shouldPlay) => {
+    setVideoPlayed(true);
+    setStopPlay(shouldPlay);
+  };
 
-    const handleClick = () => {
-        setVideoPlayed(true);
-        setStopPlay(!stopPlay)
-    };
+  const handleClick = () => {
+    handleVideoTrigger(!stopPlay);
+  };
 
-    return (
-        <div onClick={handleClick} style={{ width: '100%', height: '100%', position: 'relative', cursor: 'pointer' }}>
-            {!videoPlayed &&
-                <img src={`https://img.youtube.com/vi/${id}/sddefault.jpg`} alt="thumbnail" 
-                     style={{ width: '100%', height: '100%', borderRadius: "10px"  }}
-                />
-            }
+  // Adding keyboard event handler
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      // 'Enter' or 'Space' key
+      handleVideoTrigger(!stopPlay);
+    }
+  };
 
-            {videoPlayed && !stopPlay ? (
-                <iframe
-                    width="100%"
-                    height="300px"
-                    src={videoSrc}
-                    title="Video"
-                    allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                    frameBorder="0"
-                    allowFullScreen
-                />
-            ) : (
-                <div style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    {/* <img src="your-youtube-play-icon" alt="Play button" /> */}
-                </div>
-            )}
+  return (
+    <div
+      onClick={handleClick}
+      onKeyPress={handleKeyPress} // Keyboard event listener
+      role="button" // Role to indicate this is a button
+      tabIndex="0" // tabIndex so it's focusable
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        cursor: "pointer",
+      }}
+    >
+      {!videoPlayed && (
+        <img
+          src={`https://img.youtube.com/vi/${id}/sddefault.jpg`}
+          alt="thumbnail"
+          style={{ width: "100%", height: "100%", borderRadius: "10px" }}
+        />
+      )}
+
+      {videoPlayed && !stopPlay ? (
+        <iframe
+          width="100%"
+          height="300px"
+          src={videoSrc}
+          title="Video"
+          allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          allowFullScreen
+        />
+      ) : (
+        <div
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {/* <img src="your-youtube-play-icon" alt="Play button" /> */}
         </div>
-    );
-};
+      )}
+    </div>
+  );
+}
