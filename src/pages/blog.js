7879a709254a2525318e2a2261de662d48750c40
Will Kencel <wkencel1@gmail.com>
2024-04-14 10:37:20 -0700

    added blog main page and styling

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,38 +2,50 @@ import React from "react";
 import Layout from "../components/layout"; // Assuming you have a Layout component
 import { Link } from "gatsby";
 import "../assets/css/blogList.css"; // Assuming you are using separate CSS file for styling
+import AIWeddingImage from "../assets/images/blogImages/weddingbandAI_img1.webp";
 
 const blogPosts = [
   {
     title: "The Ultimate Wedding Band Guide",
     excerpt:
       "Planning your wedding day includes creating a magical musical atmosphere. Our Ultimate Wedding Band Guide is here to help you navigate through options, ensuring your musical backdrop is as perfect as your love story.",
-    imageUrl: "/path-to-your-image-1.jpg",
+    imageUrl: AIWeddingImage,
     blogUrl: "/the-ultimate-wedding-band-guide",
   },
   {
-    title: "Post Title 2",
-    excerpt: "This is a short description of the second blog post...",
-    imageUrl: "/path-to-your-image-2.jpg",
-    blogUrl: "/blog/post-2",
+    title: "next post coming soon",
+    excerpt: "Stay tuned for our next blog post...",
   },
-  // Add more blog posts here...
 ];
 
 const BlogList = () => {
   return (
     <Layout>
-      <h1>Check out our blog</h1>
+      <h1 style={{ "text-align": "center" }}>Check out our blog</h1>
       <div className="blog-list">
         {blogPosts.map((post, index) => (
-          <div key={index} className="blog-item">
-            <img src={post.imageUrl} alt={post.title} className="blog-image" />
-            <h2>{post.title}</h2>
-            <p>{post.excerpt}</p>
-            <Link to={post.blogUrl} className="blog-link">
-              Read More
-            </Link>
-          </div>
+          <>
+            <div key={index} className="blog-item">
+              <div>
+                {post.imageUrl ? (
+                  <img
+                    src={post.imageUrl}
+                    alt={post.title}
+                    className="blog-image"
+                  />
+                ) : null}
+              </div>
+              <div>
+                <h2>{post.title}</h2>
+                <p>{post.excerpt}</p>
+                {post.blogUrl ? (
+                  <Link to={post.blogUrl} className="blog-link">
+                    Read More
+                  </Link>
+                ) : null}
+              </div>
+            </div>
+          </>
         ))}
       </div>
     </Layout>
